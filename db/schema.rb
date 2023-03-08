3390d0a6029585c914e761f2a838029b5e6945c6
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-08 21:27:30 +0800

    Adds migration to add picture_url column to flats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_083637) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_131910) do
   create_table "flats", force: :cascade do |t|
     t.string "name", null: false
     t.string "address", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_083637) do
     t.integer "number_of_guests", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,30 @@ Flat.create!(
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 4
+  number_of_guests: 4,
+  picture_url: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 )
 Flat.create!(
   name: 'Light & Spacious Garden',
   address: '10 Clifton Gardens London',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms',
   price_per_night: 50,
-  number_of_guests: 3
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 )
 Flat.create!(
   name: 'Light & Spacious',
   address: '10 Clifton Gardens',
   description: 'A lovely summer feel for this spacious garden flat.',
   price_per_night: 40,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 )
 Flat.create!(
   name: 'Light',
   address: '10 Clifton',
   description: 'A lovely summer',
   price_per_night: 20,
-  number_of_guests: 1
+  number_of_guests: 1,
+  picture_url: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80'
 )
