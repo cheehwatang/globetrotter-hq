22a3140bd28bd9765d77afc808c3fef792502ec0
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-08 17:45:45 +0800

    Adds new and create functionality to Flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,4 +6,23 @@ class FlatsController < ApplicationController
   def show
     @flat = Flat.find(params[:id])
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path, alert: "Flat was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'flats#index'
-  resources :flats, only: %i[index show]
+  resources :flats, only: %i[index show new create]
 end
