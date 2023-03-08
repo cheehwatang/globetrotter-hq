04244e675795c563dd5aeb0cc7b3e03cce275fe3
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-08 22:17:02 +0800

    Adds functionality to input and edit the picture url

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Flat.create!(
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
   number_of_guests: 4,
-  picture_url: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  picture_url: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be'
 )
 Flat.create!(
   name: 'Light & Spacious Garden',
@@ -15,7 +15,7 @@ Flat.create!(
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms',
   price_per_night: 50,
   number_of_guests: 3,
-  picture_url: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  picture_url: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994'
 )
 Flat.create!(
   name: 'Light & Spacious',
@@ -23,7 +23,7 @@ Flat.create!(
   description: 'A lovely summer feel for this spacious garden flat.',
   price_per_night: 40,
   number_of_guests: 2,
-  picture_url: 'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  picture_url: 'https://images.unsplash.com/photo-1554995207-c18c203602cb'
 )
 Flat.create!(
   name: 'Light',
@@ -31,5 +31,5 @@ Flat.create!(
   description: 'A lovely summer',
   price_per_night: 20,
   number_of_guests: 1,
-  picture_url: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80'
+  picture_url: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9'
 )
