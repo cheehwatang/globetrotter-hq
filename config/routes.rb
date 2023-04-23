de7149354849899b76bd515b5620096895080d24
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-23 19:45:02 +0800

    Empty repository

--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  root 'flats#index'
-  resources :flats
-end
--- db/migrate/20230308131910_add_picture_url_to_flats.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureUrlToFlats < ActiveRecord::Migration[7.0]
-  def change
-    add_column :flats, :picture_url, :string, null: false
-  end
-end
