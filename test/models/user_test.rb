a4e85017b44e26b44dfb14a221f0643ffadd0b66
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-24 21:01:34 +0800

    Adds validation tests for password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  # Tests for email
   test "invalid if email is empty" do
     user = User.new
     user.valid?
@@ -17,5 +18,29 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: "test@example")
     user.valid?
     assert_not user.errors[:email].empty?
+  end
+
+  # Tests for password
+  test "invalid if password is empty" do
+    user = User.new
+    user.valid?
+    assert_not user.errors[:password].empty?
+  end
+
+  test "valid if password has length equal or greater than 8" do
+    user = User.new(password: "12345678")
+    user.valid?
+    assert_empty user.errors[:password]
+  end
+
+  test "invalid if password has length less than 8" do
+    user = User.new(password: "1234567")
+    user.valid?
+    assert_not user.errors[:password].empty?
+  end
+
+  test "password is encrypted" do
+    user = User.new(password: "password")
+    assert_not_equal "password", user.encrypted_password
   end
 end
