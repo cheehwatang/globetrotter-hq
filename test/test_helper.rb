3671cba88ee6e57464bd926d520bd2bdc37afcd7
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-24 19:56:31 +0800

    Adds test helper for Devise

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # Devise test helpers
+  include Warden::Test::Helpers
+  Warden.test_mode!
 end
 
 # Folder path for screenshots
