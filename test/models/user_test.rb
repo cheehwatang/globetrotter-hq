9ba17f85950cd03b9fa99b3c292a7e66d29432c8
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-24 20:47:59 +0800

    Adds validations testing for the email in the User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid if email is empty" do
+    user = User.new
+    user.valid?
+    assert_not user.errors[:email].empty?
+  end
+
+  test "valid if email is correctly formatted" do
+    user = User.new(email: "test@example.com")
+    user.valid?
+    assert_empty user.errors[:email]
+  end
+
+  test "invalid if email is incorrectly formatted" do
+    user = User.new(email: "test@example")
+    user.valid?
+    assert_not user.errors[:email].empty?
+  end
 end
